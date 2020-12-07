2d8081fea6302a40106ddb6fcda89ec6b7bd7f43
Brian Oliver <semperindoctrina19@gmail.com>
2020-12-07 04:03:50 -0600

    added user avatars

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -2,7 +2,19 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+def upload_avatar_image(instance, filename):
+    return f"users/profiles/{instance.user.username}/{filename}"
+
 class Profile(models.Model):
+    avatar = ProcessedImageField(
+        upload_to=upload_avatar_image, 
+        processors=[ResizeToFill(100, 100)],
+        format="JPEG",
+        options={"quality":80}
+    )
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
 
