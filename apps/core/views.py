13eecd945c982a0fa642e4e5dd67a0fa815796bf
Brian Oliver <semperindoctrina19@gmail.com>
2020-12-27 20:55:45 -0600

    redirect user to profile page after signup

--- apps/core/views.py
+++ apps/core/views.py
@@ -12,7 +12,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            return redirect('profile', username=request.user.username)
     else:
         form = UserCreationForm()
     
